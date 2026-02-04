76b6f97dccc41ebfbb58fe661696fd433ae71f12
daiki <d.a0807derude@gmail.com>
2026-02-05 08:30:57 +0900

    Update configuration for new repository URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/math-2-box-shape/',
+  base: '/ath-2-box-shape/',
 })
